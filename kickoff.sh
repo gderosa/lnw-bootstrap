3175e40fd293cfe984d429f77b40a914b26965b0
gd <Guido.DeRosa@forcepoint.com>
2024-12-04 18:52:42 +0000

    pass BRANCH env var to provisioner

--- kickoff.sh
+++ kickoff.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# ENVIRONMENT:
+# BRANCH: target app git branch to be checked out
+
 APP=lnw
 REPONAME=$APP
 GHUSER=gderosa
@@ -15,6 +18,10 @@ apt-get -y install git-core
 cd /opt
 git clone https://github.com/$GHUSER/$REPONAME.git
 cd $REPONAME
+# BRANCH might be passed as env variable
+if [ -z "$BRANCH" ]; then
+    git checkout $BRANCH
+fi
 
 sh scripts/setup.sh
 
